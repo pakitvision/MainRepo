2adde910714682497cbf0cbce276bb30e924ccff
pakitvision <pakitvision@gmail.com>
2014-12-04 17:24:24 +0500

    Release 2 Message change.

--- Company.WebUI/Items.aspx.cs
+++ Company.WebUI/Items.aspx.cs
@@ -11,7 +11,7 @@ namespace Company.WebUI
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            Response.Write("Release 1");
+            Response.Write("Release 2");
         }
     }
 }
